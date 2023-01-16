826505ee1409a6276cdbebcfce5122c4ec828245
staticGuru <vigneshguru274@gmail.com>
2023-01-16 20:13:11 +0530

    add the routers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './screens/home';
+import SavedCharacters from './screens/savedCharacters';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Route exact path="/" component={Home} />
+    <Route path="/character/:id" component={SavedCharacters} />
     </div>
+  </BrowserRouter>
   );
 }
 
---
+++ src/screens/savedCharacters.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SavedCharacters() {
+  return (
+    <div>SavedList</div>
+  )
+}
+
+export default SavedCharacters
