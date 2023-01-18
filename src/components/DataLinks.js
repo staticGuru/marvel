b6b064a95c0ed59f60caaed5c81f8d8c607431cd
staticGuru <vigneshguru274@gmail.com>
2023-01-18 17:16:14 +0530

    add the marvel sections

--- src/components/DataLinks.js
+++ src/components/DataLinks.js
@@ -3,6 +3,7 @@ import React from "react";
 function DataLinks({ items }) {
   return (
     <div className="dataLinks">
+    {/**Generate the links in the item details */}
       {items?.map((itemsData, index) => {
         return (
           <h3 className="links card-description">
